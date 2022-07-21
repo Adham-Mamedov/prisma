e27b162492339adba86bc4babb6cbd0f871dcb1f
Adkham <adhammamedov@mail.ru>
2022-07-21 20:59:50 +0500

    feat: add example for DELETE operations

--- script.ts
+++ script.ts
@@ -4,38 +4,20 @@ const prisma = new PrismaClient()
 
 async function main() {
   await prisma.$connect();
-  // ===== UPDATE OPERATIONS =====
-  const user = await prisma.user.update({
+  // ===== DELETE OPERATIONS =====
+  const deletedUser = await prisma.user.delete({
+    where: {
+      email: 'bob@gmail.com'
+    }
+  }).catch(e => console.error(e.message));
+
+  const deletedUsers = await prisma.user.deleteMany({
     where: {
       email: 'adham@gmail.com'
-    },
-    data: {
-      name: 'Adkham',
-      age: {
-        increment: 1,
-        // decrement: 1,
-        // multiply: 10,
-        // divide: 10,
-      }
-    },
-    select: {
-      name: true,
-      age: true
     }
-  })
-  const usersCount = await prisma.user.updateMany({
-    where: {
-      email: {
-        endsWith: 'gmail.com'
-      },
-      name: 'Adkham'
-    },
-    data: {
-      name: 'Adham'
-    }
-  })
+  }).catch(e => console.error(e.message))
 
-  console.log({user, usersCount})
+  console.log({users: deletedUsers})
 }
 
 main()
