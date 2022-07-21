ccedc8704daeefd327e7de52fc19ba3e97b8d5de
Adkham <adhammamedov@mail.ru>
2022-07-21 20:41:05 +0500

    feat: add example for READ operations

--- script.ts
+++ script.ts
@@ -4,53 +4,38 @@ const prisma = new PrismaClient()
 
 async function main() {
   await prisma.$connect();
-  await prisma.user.deleteMany();
-  await prisma.userPreference.deleteMany();
-  // ===== CREATE OPERATIONS =====
-
-  // const user = await prisma.user.create({
-  //   data: {
-  //     name: 'Adham',
-  //     email: 'adhammamedov@mail.ru',
-  //     age: 21,
-  //     userPreference: {
-  //       create: {
-  //         emailUpdates: true
-  //       }
-  //     }
-  //   },
-  //   select: {
-  //     email: true,
-  //     userPreference: {
-  //       select: {
-  //         id: true
-  //       }
-  //     }
-  //   }
-  //   // include: {
-  //   //   userPreference: true
-  //   // }
-  // })
-  //
-  // const preferences = await prisma.userPreference.findMany()
-  // const users = await prisma.user.findMany()
-
-  const usersCount = await prisma.user.createMany({
-    data: [
-      {
-        name: 'Adham',
-        email: 'adham@gmail.com',
-        age: 21,
-      },
-      {
-        name: 'Bob',
-        email: 'bob@gmail.com',
-        age: 36,
-      }
-    ]
+  // ===== READ OPERATIONS =====
+  // const users = await prisma.user.findMany();
+  const user = await prisma.user.findUnique({
+    where: {
+      email: 'adham@gmail.com'
+    }
   })
 
-  console.log({usersCount})
+  const users = await prisma.user.findMany({
+    where: {
+      name: {
+        equals: 'Adham',
+      },
+      email: {
+        contains: 'test',
+        mode: 'insensitive',
+      },
+      writtenPosts: {
+        some: {
+          averageRating: {gt: 4}
+        }
+      }
+    },
+    // skip: 1,
+    // take: 2,
+    // orderBy: {
+    //   age: "desc"
+    // },
+    // distinct: ["name", "age"]
+  });
+
+  console.log({users, user})
 }
 
 main()
